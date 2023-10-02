acf08340a5742b6385913d21ea7e5bc609780059
Diegogete <ddrsilva@senacrs.com.br>
2023-10-02 08:16:33 -0300

    ajuste

--- assets/js/telaInicial.js
+++ assets/js/telaInicial.js
@@ -1,10 +1,16 @@
-$('.botao-fechar').click(function(){
+// $('.botao-fechar').click(function(){
+//   $('.backdrop').fadeOut()
+//   const stopVideos = () => {
+//     document.querySelectorAll('iframe').forEach(v => { v.src = v.src });
+//     document.querySelectorAll('video').forEach(v => { v.pause() });
+//   };
+//   stopVideos();
+// })
+// $('.btVideo').click(function(){
+//   $('.backdrop').fadeIn()
+// })
+$('#fechar-video').click(function(){
   $('.backdrop').fadeOut()
-  const stopVideos = () => {
-    document.querySelectorAll('iframe').forEach(v => { v.src = v.src });
-    document.querySelectorAll('video').forEach(v => { v.pause() });
-  };
-  stopVideos();
 })
 $('.btVideo').click(function(){
   $('.backdrop').fadeIn()
--- assets/print/print.js
+++ assets/print/print.js
@@ -126,7 +126,7 @@
     HTMLstring+='<button class="btn btn-primary" data-click="fontSize" data-value="1">Médio</button> ';
     HTMLstring+='<button class="btn btn-primary" data-click="fontSize" data-value="2">Grande</button></span> ';
     HTMLstring+='</div>\n';
-    HTMLstring+='<img src="'+img_path+'logo_sesc_horizontal.svg" id="marcadaguaLogo">\n';
+    HTMLstring+='<img src="'+img_path+'sesccncsenac.png" id="marcadaguaLogo">\n';
     HTMLstring+='\n';
     HTMLstring+='<div id="booklet" class="visualizar-print">\n';
     HTMLstring+='<h1>' + $("#nome-curso").text() + '</h1>\n';
